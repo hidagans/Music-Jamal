6da579df57a5b5dacf19733d022b13271f9fd34d
PayXr <103725749+PayXr@users.noreply.github.com>
2022-07-24 18:12:38 +0700

    test

--- CilikMusic/plugins/sudo/gruplist.py
+++ CilikMusic/plugins/sudo/gruplist.py
@@ -13,40 +13,40 @@ from pyrogram.types import Message
 from CilikMusic import app
 from CilikMusic.misc import OWNER_ID
 from CilikMusic.utils.database import (gruplist_db,
-                                        add_gruplist,
+                                       add_gruplist,
                                        remove_gruplist)
 
 
 @app.on_message(filters.command("addgc", [".", "/"]) & filters.user(OWNER_ID)
 async def add_gruplist(client, message: Message):
     if len(message.command) != 2:
-        return await message.reply_text(_["black_1"])
+        return await message.reply_text("**Penggunaan:**\n/addgc [CHAT_ID]")
     chat_id = int(message.text.strip().split()[1])
     if chat_id in await add_gruplist():
-        return await message.reply_text(_["black_2"])
+        return await message.reply_text("Grup ini sudah gabung di **Jamal Project**")
     gruplisted = await add_gruplist(chat_id)
     if gruplisted:
-        await message.reply_text(_["black_3"])
+        await message.reply_text("✅ **Succes**\nGrup ini berhasil bergabung di **Jamal Project**")
     else:
         await message.reply_text("Sesuatu yang salah terjadi.")
 
                 
-@app.on_message(filters.command("delgc", [".", "/"]) & filters.user(OWNER_ID)
+@app.on_message(filters.command("removegc", [".", "/"]) & filters.user(OWNER_ID)
 async def remove_gruplist(client, message: Message):
     if len(message.command) != 2:
-        return await message.reply_text(_["black_4"])
+        return await message.reply_text("**Penggunaan:**\nremovegc/ [CHAT_ID]")
     chat_id = int(message.text.strip().split()[1])
     if chat_id not in await add_gruplist():
-        return await message.reply_text(_["black_5"])
+        return await message.reply_text("Grup ini sudah keluar dari **Jamal Project**")
     nogruplisted = await remove_gruplist(chat_id)
     if nogruplisted:
-        return await message.reply_text(_["black_6"])
+        return await message.reply_text("✅ **Succes**\nGrup ini berhasil dikeluarkan dari **Jamal Project**")
     await message.reply_text("Sesuatu yang salah terjadi.")
 
 
 @app.on_message(filters.command("gruplist", [".", "/"]) & filters.user(OWNER_ID)
 async def all_gruplist(client, message: Message):
-    text = _["black_7"]
+    text = "**Grup Yang Bergabung di Jamal Project:**\n\n"
     j = 0
     for count, chat_id in enumerate(await gruplist_db(), 1):
         try:
@@ -56,6 +56,6 @@ async def all_gruplist(client, message: Message):
         j = 1
         text += f"**{count}. {title}** [`{chat_id}`]\n"
     if j == 0:
-        await message.reply_text(_["black_8"])
+        await message.reply_text("**Tidak ada yang bergabung di Jamal project**")
     else:
         await message.reply_text(text)
