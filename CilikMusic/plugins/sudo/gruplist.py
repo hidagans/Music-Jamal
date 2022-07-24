7b3c731d64a91a5afa84e336a0b61c3ada4b000e
PayXr <103725749+PayXr@users.noreply.github.com>
2022-07-24 17:55:15 +0700

    Update gruplist.py

--- CilikMusic/plugins/sudo/gruplist.py
+++ CilikMusic/plugins/sudo/gruplist.py
@@ -10,70 +10,42 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from config import BANNED_USERS, MONGO_DB_URI, OWNER_ID
-from strings import get_command
 from CilikMusic import app
-from CilikMusic.misc import GRUPLIST
-from CilikMusic.utils.database import add_gruplist, remove_gruplist
+from CilikMusic.misc import OWNER_ID
+from CilikMusic.utils.database import (gruplist_db,
+                                        add_gruplist,
+                                       remove_gruplist)
 
 
+@app.on_message(filters.command("addgc", [".", "/"]) & filters.user(OWNER_ID)
+async def blacklist_chat_func(client, message: Message):
+    if len(message.command) != 2:
+        return await message.reply_text(_["black_1"])
+    chat_id = int(message.text.strip().split()[1])
+    if chat_id in await blacklisted_chats():
+        return await message.reply_text(_["black_2"])
+    blacklisted = await blacklist_chat(chat_id)
+    if blacklisted:
+        await message.reply_text(_["black_3"])
+    else:
+        await message.reply_text("Sesuatu yang salah terjadi.")
 
-@app.on_message(
-    filters.command("addgc", [".", "/"]) & filters.user(OWNER_ID)
-)
-async def grupadd(client, message: Message):
-    if MONGO_DB_URI is None:
-        return await message.reply_text(
-            "**Karena masalah privasi bot, Anda tidak dapat mengelola pengguna sudo saat menggunakan Database Cilik.\n\n Silakan isi MONGO_DB_URI Anda di vars Anda untuk menggunakan fitur ini**"
-        )
-    if not message.reply_to_message:
-        if len(message.command) != 2:
-            return await message.reply_text("**Berikan id Grup Yang ingin ditambahkan di Jamal Project**")
-        chat = message.text.split(None, 1)[1]
-        if "@" in chat:
-            chat = chat.replace("@", "")
-        chat = await app.get_chat(chat)
-        if chat.id in GRUPLIST:
-            return await message.reply_text(
-                f"Grup {chat.mention} sudah bergabung di Jamal Project"
-            )
-        added = await add_gruplist(chat.id)
-        if added:
-            GRUPLIST.add(chat.id)
-            await message.reply_text(f"{chat.mention} Ditambahkan ke Jamal Project.")
-        else:
-            await message.reply_text("Failed")
-        return
-
-
-@app.on_message(
-    filters.command("delgc", [".", "/"]) & filters.user(OWNER_ID)
-)
-async def grupdel(client, message: Message):
-    if MONGO_DB_URI is None:
-        return await message.reply_text(
-            "**Karena masalah privasi bot, Anda tidak dapat mengelola pengguna sudo saat menggunakan Database Cilik.\n\n Silakan isi MONGO_DB_URI Anda di vars Anda untuk menggunakan fitur ini**"
-        )
-    if not message.reply_to_message:
-        if len(message.command) != 2:
-            return await message.reply_text("**Berikan id Grup Yang ingin dihapus di Jamal Project**")
-        chat = message.text.split(None, 1)[1]
-        if "@" in chat:
-            chat = chat.replace("@", "")
-        chat = await app.get_chat(chat)
-        if chat.id not in GRUPLIST:
-            return await message.reply_text("Grup ini Bukan bagian dari Jamal Project")
-        removed = await remove_gruplist(chat.id)
-        if removed:
-            GRUPLIST.remove(chat.id)
-            await message.reply_text("Dikeluarkan dari Jamal Project")
-            return
-        await message.reply_text(f"Sesuatu yang salah terjadi.")
-        return
+                
+@app.on_message(filters.command("delgc", [".", "/"]) & filters.user(OWNER_ID)
+async def white_funciton(client, message: Message):
+    if len(message.command) != 2:
+        return await message.reply_text(_["black_4"])
+    chat_id = int(message.text.strip().split()[1])
+    if chat_id not in await blacklisted_chats():
+        return await message.reply_text(_["black_5"])
+    whitelisted = await whitelist_chat(chat_id)
+    if whitelisted:
+        return await message.reply_text(_["black_6"])
+    await message.reply_text("Sesuatu yang salah terjadi.")
 
 
 @app.on_message(filters.command("gruplist", [".", "/"]) & filters.user(OWNER_ID)
-async def sudoers_list(client, message: Message, _):
+async def all_chats(client, message: Message):
     text = _["black_7"]
     j = 0
     for count, chat_id in enumerate(await blacklisted_chats(), 1):
