cbfb9a1704348a31302a7927cb69db7326377833
PayXr <103725749+PayXr@users.noreply.github.com>
2022-07-24 17:59:35 +0700

    Update gruplist.py

--- CilikMusic/plugins/sudo/gruplist.py
+++ CilikMusic/plugins/sudo/gruplist.py
@@ -18,37 +18,37 @@ from CilikMusic.utils.database import (gruplist_db,
 
 
 @app.on_message(filters.command("addgc", [".", "/"]) & filters.user(OWNER_ID)
-async def blacklist_chat_func(client, message: Message):
+async def add_gruplist(client, message: Message):
     if len(message.command) != 2:
         return await message.reply_text(_["black_1"])
     chat_id = int(message.text.strip().split()[1])
-    if chat_id in await blacklisted_chats():
+    if chat_id in await add_gruplist():
         return await message.reply_text(_["black_2"])
-    blacklisted = await blacklist_chat(chat_id)
-    if blacklisted:
+    gruplisted = await add_gruplist(chat_id)
+    if gruplisted:
         await message.reply_text(_["black_3"])
     else:
         await message.reply_text("Sesuatu yang salah terjadi.")
 
                 
 @app.on_message(filters.command("delgc", [".", "/"]) & filters.user(OWNER_ID)
-async def white_funciton(client, message: Message):
+async def remove_gruplist(client, message: Message):
     if len(message.command) != 2:
         return await message.reply_text(_["black_4"])
     chat_id = int(message.text.strip().split()[1])
-    if chat_id not in await blacklisted_chats():
+    if chat_id not in await add_gruplist():
         return await message.reply_text(_["black_5"])
-    whitelisted = await whitelist_chat(chat_id)
-    if whitelisted:
+    nogruplisted = await remove_gruplist(chat_id)
+    if nogruplisted:
         return await message.reply_text(_["black_6"])
     await message.reply_text("Sesuatu yang salah terjadi.")
 
 
 @app.on_message(filters.command("gruplist", [".", "/"]) & filters.user(OWNER_ID)
-async def all_chats(client, message: Message):
+async def all_gruplist(client, message: Message):
     text = _["black_7"]
     j = 0
-    for count, chat_id in enumerate(await blacklisted_chats(), 1):
+    for count, chat_id in enumerate(await gruplist_db(), 1):
         try:
             title = (await app.get_chat(chat_id)).title
         except Exception:
